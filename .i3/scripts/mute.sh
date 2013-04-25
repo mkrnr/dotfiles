cacfa5bde628433ba1dfc0ea90b4aeebe9a65e05
mkrnr <info@mkoerner.de>
2013-04-26 00:51:58 +0200

    Cosmetics

--- .i3/scripts/mute.sh
+++ .i3/scripts/mute.sh
@@ -3,7 +3,7 @@
 # This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
 # More information: http://creativecommons.org/licenses/by-sa/3.0/
 #
-# Author: Martin Koerner (http://mkoerner.de/)
+# Author: Martin Koerner <http://mkoerner.de/>
 
 CURRENT_STATE=`amixer get Master | egrep 'Playback.*?\[o' | egrep -o '\[o.+\]'`
 if [[ $CURRENT_STATE == '[on]' ]]; then
