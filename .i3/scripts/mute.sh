dae2f78dcd852313a9b78c194e5d621d9188cb8c
mkrnr <info@mkoerner.de>
2013-04-25 23:42:46 +0200

    Add i3 config files

---
+++ .i3/scripts/mute.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+# More information: http://creativecommons.org/licenses/by-sa/3.0/
+#
+# Author: Martin Koerner (http://mkoerner.de/)
+
+CURRENT_STATE=`amixer get Master | egrep 'Playback.*?\[o' | egrep -o '\[o.+\]'`
+if [[ $CURRENT_STATE == '[on]' ]]; then
+ amixer set Master 0+ mute
+else
+ amixer set Master unmute
+ amixer set Speaker unmute
+ amixer set Headphone unmute
+fi
